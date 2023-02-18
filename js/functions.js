ce7376566071bcabec9ec31764adc55598ae91c4
Александр <stemshadow@mail.ru>
2023-02-18 14:15:29 +0200

    добавил функцию для проверки строки на палиндром

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,8 @@ function checkLengthString(string, maxValue) {
   return (string.length <= maxValue);
 }
 
+//Функция для проверки, является ли строка палиндромом.
+function checkStringPolindrom(string){
+  let newString = string.split('').reverse().join('');
+  return (string.toLowerCase().replaceAll(' ','') === newString.toLowerCase().replaceAll(' ',''));
+}
