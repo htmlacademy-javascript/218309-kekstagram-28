d57ee73afd0ae636c41c31d151f197661db701c6
Александр <stemshadow@mail.ru>
2023-02-18 22:50:41 +0200

    Добавил ф-ию для извлечения цифр

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,19 @@ function checkStringPolindrom(string){
   let newString = string.split('').reverse().join('');
   return (string.toLowerCase().replaceAll(' ','') === newString.toLowerCase().replaceAll(' ',''));
 }
+
+/*Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
+и возвращает их в виде целого положительного числа.
+Если в строке нет ни одной цифры, функция должна вернуть NaN:*/
+function getNumbers(string){
+  let newNumber = '';
+  for(let i = 0;i < string.length;i++){
+    if(string[i] == Number(string[i]) && string[i] !== ' '){
+      newNumber += string[i];
+    }
+  }
+  if(newNumber === ''){
+    return NaN;
+  }
+  return Number(newNumber);
+}
