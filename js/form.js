112b9c9e05e823789f4b422e2d464afc5033a28f
Александр <stemshadow@mail.ru>
2023-03-23 18:37:26 +0200

    чутка поправил

--- js/form.js
+++ js/form.js
@@ -6,85 +6,86 @@ const uploadForm = document.querySelector('#upload-select-image');
 const textDescription = uploadForm.querySelector('.text__description');
 const textHashtags = uploadForm.querySelector('.text__hashtags');
 
-const pristine = new Pristine(uploadForm, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextClass: 'img-upload__error-text',
-});
+export const validateForm = () => {
+  const pristine = new Pristine(uploadForm, {
+    classTo: 'img-upload__field-wrapper',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextClass: 'img-upload__error-text',
+  });
 
-const isEscapeKey = (evt) => evt.key === 'Escape';
+  const isEscapeKey = (evt) => evt.key === 'Escape';
 
-const onUploadEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
+  const onUploadEscKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      closeUploadPicture();
+    }
+  };
+
+  function closeUploadPicture() {
+    // resetScale();
+    // resetEffects();
+    uploadForm.reset();
+    pristine.reset();
+    imgUploadOpen.classList.add('hidden');
+    document.removeEventListener('keydown', onUploadEscKeydown);
+    elBody.classList.remove('modal-open');
+  }
+
+  function openUpLoadPicture() {
+    imgUploadOpen.classList.remove('hidden');
+    elBody.classList.add('modal-open');
+    document.addEventListener('keydown', onUploadEscKeydown);
+  }
+
+  imgUploadClose.addEventListener('click', closeUploadPicture);
+  imgUploadFile.addEventListener('change', openUpLoadPicture);
+
+  const stopPropagation = (evt) => {
+    if (evt.key === 'Escape') {
+      evt.stopPropagation();
+    }
+  };
+
+  textDescription.addEventListener('keydown', stopPropagation);
+  textHashtags.addEventListener('keydown', stopPropagation);
+
+  const VALID_HASHTAG = /^#[a-zа-яё0-9]{1,19}$/i;
+  const ERROR_TEXT_TAG = 'Хэш-теги записаны неверно';
+
+  const isValidTag = (tag) => VALID_HASHTAG.test(tag);
+
+  const hasValidCount = (tags) => tags.length <= 5;
+
+  const hasUniqueTags = (tags) => {
+    const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+    return lowerCaseTags.length === new Set(lowerCaseTags).size;
+  };
+
+  const validateTags = (value) => {
+    const tags = value
+      .trim()
+      .split(' ')
+      .filter((tag) => tag.trim().length);
+    return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+  };
+
+  pristine.addValidator(
+    textHashtags,
+    validateTags,
+    ERROR_TEXT_TAG,
+  );
+
+  const onFormSubmit = (evt) => {
     evt.preventDefault();
-    closeUploadPicture();
-  }
+    const isValid = pristine.validate();
+    if (isValid) {
+      // uploadForm.submit();
+      // console.log('Можно отправлять');
+    } else {
+      // console.log(' Не Можно отправлять');
+    }
+  };
+
+  uploadForm.addEventListener('submit', onFormSubmit);
 };
-
-function closeUploadPicture() {
-  // resetScale();
-  // resetEffects();
-  uploadForm.reset();
-  pristine.reset();
-  imgUploadOpen.classList.add('hidden');
-  document.removeEventListener('keydown', onUploadEscKeydown);
-  elBody.classList.remove('modal-open');
-}
-
-function openUpLoadPicture() {
-  imgUploadOpen.classList.remove('hidden');
-  elBody.classList.add('modal-open');
-  document.addEventListener('keydown', onUploadEscKeydown);
-}
-
-imgUploadClose.addEventListener('click', closeUploadPicture);
-imgUploadFile.addEventListener('change', openUpLoadPicture);
-
-const stopPropagation = (evt) => {
-  if (evt.key === 'Escape') {
-    evt.stopPropagation();
-  }
-};
-
-textDescription.addEventListener('keydown', stopPropagation);
-textHashtags.addEventListener('keydown', stopPropagation);
-
-const VALID_HASHTAG = /^#[a-zа-яё0-9]{1,19}$/i;
-const ERROR_TEXT_TAG = 'Хэш-теги записаны неверно';
-
-const isValidTag = (tag) => VALID_HASHTAG.test(tag);
-
-const hasValidCount = (tags) => tags.length <= 5;
-
-const hasUniqueTags = (tags) => {
-  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
-  return lowerCaseTags.length === new Set(lowerCaseTags).size;
-};
-
-const validateTags = (value) => {
-  const tags = value
-    .trim()
-    .split(' ')
-    .filter((tag) => tag.trim().length);
-  return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
-};
-
-pristine.addValidator(
-  textHashtags,
-  validateTags,
-  ERROR_TEXT_TAG,
-);
-
-const onFormSubmit = (evt) => {
-  evt.preventDefault();
-  const isValid = pristine.validate();
-  if (isValid) {
-    // uploadForm.submit();
-    console.log('Можно отправлять');
-  } else {
-    console.log(' Не Можно отправлять');
-  }
-};
-
-uploadForm.addEventListener('submit', onFormSubmit);
-
