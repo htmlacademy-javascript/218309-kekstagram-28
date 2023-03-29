c9c8764825384690430620794d049779a04e8070
Александр <stemshadow@mail.ru>
2023-03-29 16:20:03 +0200

    вынес fetch в api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,28 @@
-const getData = (onSuccess) => {};
+const getData = (onSuccess) => {
+  fetch('https://28.javascript.pages.academy/kekstagram/data')
+    .then((response) => response.json())
+    .then((generateUserPhotoDescription) => {
+      onSuccess(generateUserPhotoDescription);
+    });
+};
 
-const sendData = (onSuccess, onFail, body) => {};
+const sendData = (onSuccess, onFail, body) => {
+  fetch(
+    'https://28.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body,
+    },
+  ).then((responce) => {
+    if (responce.ok) {
+      onSuccess();
+    } else {
+      onFail('Не удалось отправить форму. Попробуй ещё раз');
+    }
+  })
+    .catch(() => {
+      onFail('Не удалось отправить форму. Попробуй ещё раз');
+    });
+};
 
-export {getData,sendData};
+export { getData, sendData };
