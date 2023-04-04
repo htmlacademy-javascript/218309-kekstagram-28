ea76aab52e8a9440e14fe8b6b596a1aec23ea9b8
Александр <stemshadow@mail.ru>
2023-04-04 19:26:11 +0200

    Правки

--- js/main.js
+++ js/main.js
@@ -1,35 +1,9 @@
 import { getData } from './api.js';
 import { renderPosts } from './thumbnail.js';
-// import { generateUserPhotoDescription } from './mock.js';
-// import { debounce } from './util.js';
-// import { showAlert } from './util.js';
 import { validateForm, setUploadFormSubmit, closeUploadPicture } from './form.js';
-import { getData } from './api.js';
 import { showSelectedFile } from './upload-picture.js';
 
-// renderPosts(generateUserPhotoDescription());
-validateForm();
-
-// const debouncedRenderGallery = debounce(renderPosts);
-// getData(debouncedRenderGallery);
-
 getData(renderPosts);
-
+validateForm();
 setUploadFormSubmit(closeUploadPicture);
-
 showSelectedFile();
-
-// import {sortByDefault, showImgFilter, setFilterClick} from './filter.js';
-// getData()
-//   .then((posts) => {
-//     renderPosts(posts, sortByDefault);
-//     // getPictureClick(posts);
-//     showImgFilter();
-//     setFilterClick(posts);
-//   })
-//   .catch(
-//     (err) => {
-//       showAlert(err);
-//     }
-//   );
-
